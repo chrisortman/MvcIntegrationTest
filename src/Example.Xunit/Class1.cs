86ded5179122ef6d7968f5e36d91c5dc869afd43
Chris Ortman <chriso@innovsys.com>
2010-05-21 14:30:54 -0500

    Adding example xunit test

---
+++ src/Example.Xunit/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Example.Xunit
+{
+    public class SampleXunitTest : MvcIntegrationTestFramework.MvcControllerTest
+    {
+        public SampleXunitTest() 
+        {
+            InitializeAspNetRuntime();
+
+            Get("home/index");
+        }
+
+        [Fact]
+        public void Should_be_successful() 
+        {
+            Assert.Equal(200,Response.StatusCode);
+        }
+    }
+}
